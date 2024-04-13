4bc4652397d4f35c2b40486dee0c1b0ee3ac1907
fabricio <fzabra@gmail.com>
2024-04-13 18:52:36 -0300

    RMAPI-0002 add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component'
+import { SearchComponent } from './search/search.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SearchComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
