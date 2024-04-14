7c9d0a205090bb3e27356407c0e167d40483714f
fabricio <fzabra@gmail.com>
2024-04-14 07:47:43 -0300

    RMAPI-0004

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.less'
 })
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatButtonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.less'
 })
